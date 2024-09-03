61b383636fee52addfabb54ca9ab6d080d25cea2
JaxsonM <tigertownball@gmail.com>
2024-09-03 16:41:38 -0400

    High Stakes Early Season

--- src/main.cpp
+++ src/main.cpp
@@ -251,6 +251,7 @@ void autonomous() {
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
     Intake.set_brake_mode(pros::MotorBrake::coast);
+    Lift.set_brake_mode(pros::MotorBrake::hold);
 
 	while (true) {
 
