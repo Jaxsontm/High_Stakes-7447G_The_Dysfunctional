ba82c40f0172a4809896d1db131ba8c0ae91243d
JaxsonM <tigertownball@gmail.com>
2024-09-21 11:30:36 -0400

    PID

--- include/importants.h
+++ include/importants.h
@@ -85,5 +85,3 @@ extern void state_machine();
 extern void state_machine_mogo();
 extern void request_new_state(State new_state);
 extern void request_new_state_mogo(StateMogo new_state);
-
-extern void LiftPID(double targetAngle);
