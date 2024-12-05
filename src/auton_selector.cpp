eaf888b51eb356a8ca0db6be068c949648862cbe
JaxsonM <tigertownball@gmail.com>
2024-12-04 20:49:06 -0500

    "Removed unused UI elements and cleaned up code in auton_selector.cpp"

--- src/auton_selector.cpp
+++ src/auton_selector.cpp
@@ -358,27 +358,25 @@ void selector() {
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  /*lv_obj_t *placement_label = lv_label_create(lv_scr_act());
+  lv_obj_t *placement_label = lv_label_create(lv_scr_act());
   lv_obj_align(placement_label, LV_ALIGN_BOTTOM_RIGHT, -20, -10);
-  lv_obj_t *placement_cover = lv_obj_create(lv_scr_act());
-  lv_obj_set_size(placement_cover, 150, 20);*/
 
   lv_obj_t *stats_label = lv_label_create(statsTab);
   lv_obj_set_style_bg_color(stats_label,
                             lv_palette_darken(LV_PALETTE_DEEP_ORANGE, 2), 0);
   lv_obj_align(stats_label, LV_ALIGN_CENTER, 0, -20);
 
-  /*cover = lv_obj_create(lv_scr_act());
+  cover = lv_obj_create(lv_scr_act());
   lv_obj_set_size(cover, 70, 40);
   lv_obj_align(cover, LV_ALIGN_LEFT_MID, 120, 30);
   lv_obj_set_style_bg_color(cover, lv_palette_darken(LV_PALETTE_DEEP_ORANGE, 2), 0);
   lv_obj_set_style_border_color(cover, lv_palette_darken(LV_PALETTE_DEEP_ORANGE, 2), 0);
-  lv_obj_add_flag(cover, LV_OBJ_FLAG_HIDDEN);*/
+  lv_obj_add_flag(cover, LV_OBJ_FLAG_HIDDEN);
 
   char posText[150];
   char statText[150];
 
-  /*while (true) {
+  while (true) {
     // always on screen
     lv_label_set_text(placement_label, "");
     lemlib::Pose trackerPos = chassis.getPose();
@@ -397,5 +395,5 @@ void selector() {
     lv_label_set_text(stats_label, statText);
 
     pros::delay(500);
-  }*/ //while loop
+  } //while loop
 } //void selector
